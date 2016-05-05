e7ebdbb4d998133da484380924926f7ac8fd07f1
NatsumeJapan <mechakuchanaryuu@gmail.com>
2016-05-05 21:22:51 +0900

    add

---
+++ contactthanks.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Contact Thank You | Really Cool Customs</title>
+	<meta name="description" content="Mina Test Site Contact Thank You">
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/contactstyle.css" />
+</head>
+
+<body>
+
+
+
+<header>
+	<div class="wrap-header zerogrid">
+		<div id="logo"><a href="index.html"><img src="./images/logo.png"/></a></div>
+		<div class="social">
+			<ul>
+				<li><a href="Facebook Page"><img src="./images/social/facebook-icon.png" /></a></li>
+				<li><a href="Google Account"><img src="./images/social/google-icon.png" /></a></li>
+				<li><a href="Twitter Feed"><img src="./images/social/twitter-bird-icon.png" /></a></li>
+				<li><a href="Linked In Account"><img src="images/social/linkedin-icon.png" /></a></li>
+			</ul>
+		</div>
+
+	</div>
+</header>
+
+
+<section id="content">
+
+
+<div class="wrap-content zerogrid">
+		<div class="row block05">
+		  <div class="title">
+
+			<div class="contacttitle"><span style="font-size:44px">Thank You!</span></div>
+            </div>
+          <br>
+            <br>
+            <br>
+
+		
+			<center>
+		<h2>Your message has been sent!</h2><br />
+        <br>
+        <br>
+        
+		
+		<p><a href="contact.html">Back to Contact Form</a></p>
+        
+        
+        </div>
+        </div>
+        
+        
+	</section>
+
+
+
+
+
+
+
+	
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+</body>
+
+</html>
